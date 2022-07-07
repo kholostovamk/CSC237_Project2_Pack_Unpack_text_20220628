5a466fd50ec8b13117d500a198fd8eaba98d81a2
margo.khm <kholostova@gmail.com>
2022-07-07 17:25:17 -0500

    description of the program + #include

---
+++ Project2.cpp
@@ -0,0 +1,20 @@
+//CSC237 Project2:
+// Student: yourName 
+// Due Date: projectDueDate // Description:
+//Text Packing / Unpacking Operations
+// This program reads a text document, “packs” the ASCII characters // from that document into unsigned int variables, and outputs those variables // to another text file, formatted as hexadecimal numbers.
+// This program also reverses the process, converting the unsigned int numbers // back into a copy of the original text document.
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+
+
+
+}
